fb1a18c612753f8593d0e1014d7a256612884013
Sunny <sunny2831@hotmail.co.uk>
2018-11-24 11:58:43 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    array = self.split(/[.?!]/).reject { |string| string == ''}
+    array.count
   end
 end
